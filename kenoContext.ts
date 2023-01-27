16392ba747f0c6d2cc22e562457882d8c716ce9c
Lyndon Sutcliffe <l11vdo@gmail.com>
2023-01-27 14:57:40 +0000

    sort order

--- kenoContext.ts
+++ kenoContext.ts
@@ -1,10 +1,10 @@
 import {useContext, createContext} from 'react';
 
 type kenoData = {
-  kenoNumbers: Number[];
-  setKenoNumbers: (k: Number[]) => void;
-  kenoBet: Number;
-  setKenoBet: (k: Number) => void;
+  kenoNumbers: number[];
+  setKenoNumbers: (k: number[]) => void;
+  kenoBet: number;
+  setKenoBet: (k: number) => void;
 };
 
 export const KenoContext = createContext<kenoData>({
